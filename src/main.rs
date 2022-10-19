eba80fb28b9038b8be11b73b006d4a318ad57ba4
Aelpxy <aelpxy@protonmail.com>
2022-10-19 16:40:10 +0100

    feat: added create_session function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::process::{Stdio, Command as Cmd};
+
 fn main() {
 	println!("rctl")
+	create_session("debian")
+}
+
+fn create_session(image: &str) {
+    let mut cmd = Cmd::new("docker")
+        .args(&["run", "-it", "-m", "128m", "--cpus", "1", &image, "bash"])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
+
+    let status = cmd.wait();
+    println!("Exited with status {:?}", status);
 }
